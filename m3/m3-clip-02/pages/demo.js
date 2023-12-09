00e87feab181125d66a226f491a494370eca5752
Sanparty <88841545+Sanparty@users.noreply.github.com>
2023-12-09 11:14:14 -0500

    added usestate

--- m3/m3-clip-02/pages/demo.js
+++ m3/m3-clip-02/pages/demo.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 export default function Demo() {
   const [text1, setText1] = useState("First");
+  useEffect(() => {
+    document.title = `${text1.length}`;
+  });
+  
   const [text2, setText2] = useState("Last");
 
   return (
     <div className="container">
       <h3>Simple State and Lifecycle Management</h3>
-
+      
       <input onChange={(e) => setText1(e.target.value)} value={text1} />
       <hr />
       <input onChange={(e) => setText2(e.target.value)} value={text2} />
